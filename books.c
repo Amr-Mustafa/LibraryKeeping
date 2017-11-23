d7f1920dec2b30b2cdb0e2f7a8a1c18282266256
Amr-Mustafa <amr.mustafa.97@outlook.com>
2017-11-23 20:04:17 +0200

    insert_book and main function updated along with helper functions.

--- books.c
+++ books.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
 #include "books.h"
 
 /*
@@ -80,7 +82,12 @@ int insert_book (void) {
     initialize_book(book, tk_book_info);
 
     /* validate the read book */
-    validate_book(book, books_database);
+    if (validate_book(book, books_database) == 1) {
+        fprintf(stderr, "ERROR: A book with the exact ISBN already exists.\n");
+        printf("You will be redirected in 3 seconds...\n");
+        sleep(3);
+        exit(EXIT_FAILURE);
+    }
 
     /****************************************************************/
     /** 5. Store the book information string in the database file. **/
@@ -91,6 +98,10 @@ int insert_book (void) {
         fprintf(stderr, "ERROR: Couldn't add the book.\n");
         exit(EXIT_FAILURE);
     }
+
+    printf("Book added successfully.\n");
+    printf("You will be redirected in 3 seconds...\n");
+    sleep(3);
 
     /*********************************/
     /** 6. Close the database file. **/
@@ -119,6 +130,11 @@ char* get_book_info (void) {
     // allocate enough memory to store the book info string
     char* book_info = (char*) malloc(sizeof(char) * 150);
 
+    // skip whitespaces and read the label
+    char c;
+    while(!isalpha(c = getchar()));
+    ungetc(c, stdin);
+
     // read user input
     fgets(book_info, 150, stdin);
 
@@ -142,57 +158,73 @@ int initialize_book (Book* book, char* book_info) {
     // We need a pointer to handle the token.
     char* token = NULL;
 
-    if (token = strtok(book_info, ","))
+    if (token = strtok(book_info, ",")) {
+        //printf("title: %s\n", token);
         strcpy(book->title, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("author: %s\n", token);
         strcpy(book->author, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("publisher: %s\n", token);
         strcpy(book->publisher, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
         strcpy(book->ISBN, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("date_of_publication: %s\n", token);
         strcpy(book->date_of_publication, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("number_of_copies: %s\n", token);
         book->number_of_copies = (unsigned int) atoi(token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("number_of_available_copies: %s\n", token);
         book->number_of_available_copies = (unsigned int) atoi(token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (token = strtok(NULL, ","))
+    if (token = strtok(NULL, ",")) {
+        //printf("Category: %s\n", token);
+        token[strlen(token) - 1] = '\0';
         strcpy(book->category, token);
+    }
     else {
         fprintf(stderr, "ERROR: Couldn't process the book info.\n");
         exit(EXIT_FAILURE);
@@ -229,9 +261,11 @@ int validate_book (Book* book, FILE* books_database) {
         // initialize the current book in the current iteration with the read book info string
         initialize_book(&book_d, book_info);
 
+        //printf("%s\n", book->ISBN);
+        //sleep(30);
+
         // make sure the ISBN of the book to be validated in unique
         if (strcmp(book_d.ISBN, book->ISBN) == 0) {
-            fprintf(stderr, "ERROR: A book with the exact ISBN already exists.\n");
 
             // return 1 for failure
             return 1;
--- main.c
+++ main.c
@@ -1,20 +1,177 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "books.h"
+
+void open_main_menu(void);
+void open_book_menu(void);
+char get_main_label(void);
+void do_main_label(char);
+char get_book_label (void);
+void do_book_label(char);
 
 int main() {
 
-    while (1) {
-
-        // Show menu.
-        show_menu();
-
-        // Read user input.
-        char label = get_label();
-
-        // Respond accordingly.
-        do_label(label, plane, 12);
-
-    }
+    // open main menu
+    open_main_menu();
 
     return 0;
+}
+
+void open_main_menu() {
+
+    while(1) {
+        // clear the screen
+        system("clear");
+
+        // show main menu
+        printf("To choose an option, enter its letter label:\n\
+        a) Book Management\n\
+        b) Member Management\n\
+        c) Borrow Management\n\
+        d) Administrative Actions\n\
+        e) Save Changes\n\
+        f) Save and Exit\n\
+        g) Exit without Saving\n");
+
+        // wait for user input
+        char label = get_main_label();
+
+        // respond accordingly
+        do_main_label(label);
+    }
+
+}
+
+void open_book_menu() {
+
+    while(1) {
+        // clear the screen
+        system("clear");
+
+        // show book menu
+        printf("To choose an option, enter its letter label:\n\
+        a) Insert a new book\n\
+        b) Search for a book\n\
+        c) Add new copies\n\
+        d) Delete a book\n\
+        e) Edit a book\n\
+        f) <-- back\n");
+
+        // wait for user input
+        char label = get_book_label();
+
+        // respond accordingly
+        do_book_label(label);
+    }
+}
+
+/*
+ * Function:  get_main_label
+ * -------------------------
+ * Reads in the label from the user and validate it.
+ *
+ *  returns: a copy of the label read.
+ */
+char get_main_label (void) {
+
+    // Store label input.
+    char label;
+
+    // Skip whitespaces and read the label.
+    while(!isalpha((label = getchar())));
+
+    // Validate input.
+    if (label < 'a' || label > 'g') {
+        fprintf(stderr, "ERROR: Invalid input.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Return the label.
+    return label;
+}
+
+/*
+ * Function:  get_book_label
+ * -------------------------
+ * Reads in the label from the user and validate it.
+ *
+ *  returns: a copy of the label read.
+ */
+char get_book_label (void) {
+
+    // Store label input.
+    char label;
+
+    // Skip whitespaces and read the label.
+    while(!isalpha((label = getchar())));
+
+    // Validate input.
+    if (label < 'a' || label > 'f') {
+        fprintf(stderr, "ERROR: Invalid input.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Return the label.
+    return label;
+}
+
+/*
+ * Function:  do_main_label
+ * -------------------------
+ * Reads in the label from the user and validate it.
+ *
+ *  returns: a copy of the label read.
+ */
+void do_main_label(char label) {
+
+    switch (label) {
+        case 'a':
+            open_book_menu();
+            break;
+
+        case 'b':
+            break;
+
+        case 'c':
+            break;
+
+        case 'd':
+            break;
+
+        case 'e':
+            break;
+    }
+}
+
+/*
+ * Function:  do_book_label
+ * -------------------------
+ * Reads in the label from the user and validate it.
+ *
+ *  returns: a copy of the label read.
+ */
+void do_book_label(char label) {
+
+    switch (label) {
+        case 'a':
+            insert_book();
+            break;
+
+        case 'b':
+            break;
+
+        case 'c':
+            break;
+
+        case 'd':
+            break;
+
+        case 'e':
+            break;
+
+        case 'f':
+            open_main_menu();
+            break;
+    }
 }
